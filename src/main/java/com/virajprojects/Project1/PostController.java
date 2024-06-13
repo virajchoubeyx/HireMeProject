269e6b6bfe467ed16e27b4c60039760b5130e6af
Viraj <virajchoubey.workspace@gmail.com>
2024-06-14 01:47:01 +0530

    good night day 1

--- src/main/java/com/virajprojects/Project1/PostController.java
+++ src/main/java/com/virajprojects/Project1/PostController.java
@@ -1,5 +1,16 @@
 package com.virajprojects.Project1;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class PostController {
 
+    @RequestMapping(value="/")
+    public void redirect(HttpServletResponse response) throws IOException {
+            response.sendRedirect("/swagger-ui.html");
+    }
 }
